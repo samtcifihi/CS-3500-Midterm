9df499f1df40e8092eab1786901641f5b44af977
jDamvel <jdamvel@gmail.com>
2019-04-14 03:31:07 -0400

    Modify about.php to include google map api.

--- Midterm_team1/php/about.php
+++ Midterm_team1/php/about.php
@@ -7,7 +7,17 @@
 
  	<link rel="stylesheet" type="text/css" href="../css/bootstrap.css" />
  	<link rel="stylesheet" type="text/css" href="../css/styles.css" />
-
+ 	<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+ 	<style>
+        #map {
+        	height: 400px;  /* The height is 400 pixels */
+        	width: 100%;  /* The width is the width of the web page */
+       	}
+   </style>
     <title>About Basic Knots</title>	
 </head>
 
@@ -28,6 +38,29 @@
 			<br>
 		</div>
 	</div>
+	<div class="row">
+		<div class="col">
+			<h5>Find some rope!</h5>
+			<br>
+			<p>This map is centered on some sporting goods stores in Kalamazoo. Use it to help find your way to a outdoor sporting goods store.
+			<div id="map"></div>
+		    <script>
+			// Initialize and add the map
+			function initMap() {
+			  // The location of Uluru
+			  var uluru = {lat: 42.27185925, lng: -85.65297853914947};
+			  // The map, centered at Uluru
+			  var map = new google.maps.Map(
+			      document.getElementById('map'), {zoom: 16, center: uluru});
+			  // The marker, positioned at Uluru
+			  var marker = new google.maps.Marker({position: uluru, map: map});
+			}
+		    </script>
+		    <script async defer
+		    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD6kCSsa0Sffk_Nd1FBwSb4JLgU8Ldp6f4&callback=initMap">
+		    </script>
+		</div>
+	</div>		
 </div>
 <!-- End of Content -->
 
