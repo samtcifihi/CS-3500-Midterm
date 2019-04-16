38e342bb4e3c8d9fb808ba4c1398cdf5a19fd2c7
samtcifihi <la.lojban@gmail.com>
2019-04-16 08:05:30 -0400

    db Working

--- Midterm_team1/raw/knots.sql
+++ Midterm_team1/raw/knots.sql
@@ -7,10 +7,13 @@
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
+SET SQL_MODE
+= "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT
+= 0;
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone
+= "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -28,11 +31,16 @@ SET time_zone = "+00:00";
 -- Table structure for table `comments`
 --
 
-CREATE TABLE `comments` (
-  `id` int(11) NOT NULL,
-  `name` varchar(50) COLLATE latin1_general_ci NOT NULL,
-  `email` varchar(50) COLLATE latin1_general_ci NOT NULL,
-  `subject` varchar(255) COLLATE latin1_general_ci NOT NULL,
+CREATE TABLE `comments`
+(
+  `id` int
+(11) NOT NULL,
+  `name` varchar
+(50) COLLATE latin1_general_ci NOT NULL,
+  `email` varchar
+(50) COLLATE latin1_general_ci NOT NULL,
+  `subject` varchar
+(255) COLLATE latin1_general_ci NOT NULL,
   `submittime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
@@ -40,7 +48,10 @@ CREATE TABLE `comments` (
 -- Dumping data for table `comments`
 --
 
-INSERT INTO `comments` (`id`, `name`, `email`, `subject`, `submittime`) VALUES
+INSERT INTO `comments` (`
+id`,
+`name
+`, `email`, `subject`, `submittime`) VALUES
 (1, 'easasdasd', 'test@test.com', 'i want to leave a comment', '2019-04-15 02:12:45'),
 (2, 'hurray', 'ourdb@isworking.com', 'your comment submitted successfully!!! wooooooT', '2019-04-15 02:13:34'),
 (3, 'rob van dammm', 'snoop@dawooop.com', 'buffer content for sure, we need more knots all day.', '2019-04-15 02:14:31');
@@ -53,7 +64,8 @@ INSERT INTO `comments` (`id`, `name`, `email`, `subject`, `submittime`) VALUES
 -- Indexes for table `comments`
 --
 ALTER TABLE `comments`
-  ADD PRIMARY KEY (`id`);
+ADD PRIMARY KEY
+(`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -63,7 +75,8 @@ ALTER TABLE `comments`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int
+(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
